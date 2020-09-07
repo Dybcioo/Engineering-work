5e7abf1f8308139827bc61e012492327db1d7b6a
Dybcio <dybowski.dominikk@gmail.com>
2020-09-07 20:05:43 +0200

    [add] btn color click

--- SuperKurier/SuperKurier/MainWindow.xaml.cs
+++ SuperKurier/SuperKurier/MainWindow.xaml.cs
@@ -20,14 +20,66 @@ namespace SuperKurier
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public string BackgroundOption { get; set; }
+        public string ForegroundOption { get; set; }
+        public Color colorBtn;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            BlackAndWhiteLayout();
+        }
+        private void BlackAndWhiteLayout(bool black = true)
+        {
+            if (black)
+            {
+                BackgroundOption = "Black";
+                ForegroundOption = "White";
+                colorBtn = Color.FromArgb(100, 104, 104, 104);
+            }
+            else
+            {
+                BackgroundOption = "White";
+                ForegroundOption = "Black";
+                colorBtn = Color.FromArgb(100, 193, 193, 193);
+            }
+        }
+        private void BtnBackgroundColor(Button btn)
+        {
+            BtnParcel.Background = Brushes.Transparent;
+            BtnEmployee.Background = Brushes.Transparent;
+            BtnRegion.Background = Brushes.Transparent;
+            BtnWarehouse.Background = Brushes.Transparent;
+            BtnTransport.Background = Brushes.Transparent;
+            BtnSettings.Background = Brushes.Transparent;
+            btn.Background = new SolidColorBrush(colorBtn);
         }
 
-        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        private void BtnParcel_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            BtnBackgroundColor(BtnParcel);
+        }
+        private void BtnEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            BtnBackgroundColor(BtnEmployee);
+        }
+        private void BtnRegion_Click(object sender, RoutedEventArgs e)
+        {
+            BtnBackgroundColor(BtnRegion);
+        }
+        private void BtnWarehouse_Click(object sender, RoutedEventArgs e)
+        {
+            BtnBackgroundColor(BtnWarehouse);
+        }
+        private void BtnTransport_Click(object sender, RoutedEventArgs e)
+        {
+            BtnBackgroundColor(BtnTransport);
+        }
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            BtnBackgroundColor(BtnSettings);
         }
     }
 }
