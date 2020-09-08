dcf5d3ab14d506104836dde5d1d49e49e44a83b8
Dybcio <dybowski.dominikk@gmail.com>
2020-09-08 21:12:31 +0200

    [add] test connection sql

---
+++ SuperKurier/ConnectionSQL/Connection.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectionSQL
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ConnectionSQL"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ConnectionSQL;assembly=ConnectionSQL"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class Connection : Control
+    {
+        public string ConnectionString { get; set; }
+        static Connection()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Connection), new FrameworkPropertyMetadata(typeof(Connection)));
+        }
+
+        public Connection(string serverDb, string db, string user, string password)
+        {
+           ConnectionString = GetConnectionString(serverDb, db, user, password);
+        }
+
+        public bool ConnectionSql()
+        {
+            try
+            {
+                SqlConnection cnn = new SqlConnection(ConnectionString);
+                cnn.Open();
+            }catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private string GetConnectionString(string serverDb, string db, string user, string password)
+        {
+            StringBuilder sb = new StringBuilder("Data Source=");
+            sb.Append(serverDb);
+            sb.Append(";Initial Catalog=");
+            sb.Append(db);
+            sb.Append(";User ID=");
+            sb.Append(user);
+            sb.Append(";Password=");
+            sb.Append(password);
+            return sb.ToString();
+        }
+    }
+}
--- SuperKurier/SuperKurier/MainWindow.xaml.cs
+++ SuperKurier/SuperKurier/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ConnectionSQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +27,9 @@ namespace SuperKurier
         public string ForegroundOption { get; set; }
         public string InputOption { get; set; }
         public Color ColorBtn { get; set; }
+        public string TestConnection { get; set; }
+        public string ColorConnection { get; set; }
+        public Connection Conn { get; set; }
 
         public MainWindow()
         {
@@ -32,6 +37,8 @@ namespace SuperKurier
             DataContext = this;
             BlackAndWhiteLayout();
             GetDBSettings();
+
+           // BtnTestConn_Click(null, null);
         }
         private void BlackAndWhiteLayout(bool black = true)
         {
@@ -49,6 +56,10 @@ namespace SuperKurier
                 InputOption = "#FF6EAAFF";
                 ColorBtn = Color.FromArgb(100, 193, 193, 193);
             }
+            ResetContext();
+        }
+        private void ResetContext()
+        {
             DataContext = null;
             DataContext = this;
         }
@@ -100,7 +111,7 @@ namespace SuperKurier
             Properties.Settings.Default.DBServer = DBServer.Text;
             Properties.Settings.Default.DBData = DBData.Text;
             Properties.Settings.Default.DBUser = DBUser.Text;
-            Properties.Settings.Default.DBPassword = DBPassword.Text;
+            Properties.Settings.Default.DBPassword = DBPassword.Password;
             Properties.Settings.Default.Save();
         }
 
@@ -109,12 +120,42 @@ namespace SuperKurier
             DBServer.Text = Properties.Settings.Default.DBServer;
             DBData.Text = Properties.Settings.Default.DBData;
             DBUser.Text = Properties.Settings.Default.DBUser;
-            DBPassword.Text = Properties.Settings.Default.DBPassword;
+            DBPassword.Password = Properties.Settings.Default.DBPassword;
         }
 
         private void BtnSaveDBSettings_Click(object sender, RoutedEventArgs e)
         {
             SaveDBSettings();
+        }
+
+        private void BtnTestConn_Click(object sender, RoutedEventArgs e)
+        {
+            BtnTestConn.IsEnabled = false;
+            TestConnection = "";
+            ResetContext();
+            var t = new Thread(() =>
+            {
+                Conn = new Connection(Properties.Settings.Default.DBServer,
+                Properties.Settings.Default.DBData,
+                Properties.Settings.Default.DBUser,
+                Properties.Settings.Default.DBPassword);
+                if (Conn.ConnectionSql())
+                {
+                    TestConnection = "Udane połączenie z bazą danych";
+                    ColorConnection = "Green";
+                }
+                else
+                {
+                    TestConnection = "Nieudane połącznie z bazą danych";
+                    ColorConnection = "Red";
+                }
+                Application.Current.Dispatcher.Invoke((Action)delegate {
+                    BtnTestConn.IsEnabled = true;
+                    ResetContext();
+                });
+            });
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
         }
 
 
