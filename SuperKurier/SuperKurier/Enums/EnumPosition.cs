ab84c533d0dee9d3cf6f7d791c507482e5020932
Dybcio <dybowski.dominikk@gmail.com>
2020-12-01 20:55:52 +0100

    add enum position

---
+++ SuperKurier/SuperKurier/Enums/EnumPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperKurier.Enums
+{
+    enum EnumPosition
+    {
+        Admin = 2,
+        OfficeWorker = 3,
+        Warehouseman = 4,
+        Courier = 5
+    }
+}
