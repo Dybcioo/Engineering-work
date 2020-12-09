4dee43cc0488cbc94e498f955594a74defe5df13
Dybcio <dybowski.dominikk@gmail.com>
2020-12-09 20:18:47 +0100

    update Database

--- SuperKurier/DataModel/Employee.cs
+++ SuperKurier/DataModel/Employee.cs
@@ -31,6 +31,7 @@ namespace DataModel
         public Nullable<int> idRegion { get; set; }
         public Nullable<decimal> salary { get; set; }
         public Nullable<System.DateTime> dateOfEmployment { get; set; }
+        public bool isActive { get; set; }
     
         public virtual Address Address { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
